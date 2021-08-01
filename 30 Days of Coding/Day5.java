1d18aaa213d60ffba3e8e799a7b2ec8ddde2ab03
Elcan Mahmudov <76110178+Elcan124@users.noreply.github.com>
2021-08-01 19:53:59 +0400

    Day5
    Solution Day5

---
+++ 30 Days of Coding/Day5.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.daysofcoding;
+import java.io.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        for(int i=1 ; i<=10 ; i++){
+            
+            System.out.println( n + " x " +  i  + " = " + n*i);
+        }
+        
+        
+        
+    }
+}
