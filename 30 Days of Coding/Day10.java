9537a760714bc55597b99b1cb92d4a2cf2778373
Elcan Mahmudov <76110178+Elcan124@users.noreply.github.com>
2021-08-04 17:18:18 +0400

    Day10

---
+++ 30 Days of Coding/Day10.java
@@ -0,0 +1,49 @@
+//Problem ---------------https://www.hackerrank.com/challenges/30-binary-numbers/problem
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        
+         int rem=0,s=0,t=0;
+
+
+    while(n>0)
+        {
+        rem=n%2;
+        n=n/2;
+        if(rem==1)
+         {   s++;
+           if(s>=t)
+
+            t=s;
+
+        }
+        else{
+
+            s=0;
+        }   
+    }
+
+    System.out.println(t);
+}
+        
+        
+        
+    }
